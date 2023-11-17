dc9166aed5bbe02d1f98e83bb50f2263b52ed5d7
maria <marianava171004@gmail.com>
2023-11-17 19:45:23 +0000

    Created "WeatherApiConnector" with API call method.

---
+++ src/main/java/dacd/navarro/control/WeatherApiConnector.java
@@ -0,0 +1,24 @@
+package dacd.navarro.control;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class WeatherApiConnector {
+    public static String getApiResponse(double latitude, double longitude, String apiKey) throws IOException {
+        HttpClient httpClient = HttpClients.createDefault();
+        String apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+
+        HttpGet httpGet = new HttpGet(apiUrl);
+        HttpResponse response = httpClient.execute(httpGet);
+
+        if (response.getStatusLine().getStatusCode() == 200) {
+            return EntityUtils.toString(response.getEntity());
+        } else {
+            return null;
+        }
+    }
+}
