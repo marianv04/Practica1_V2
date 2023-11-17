bc03425938044a11422b18ab1d40b8c2a4cae181
maria <marianava171004@gmail.com>
2023-11-17 19:37:16 +0000

    Added "Location" and "WeatherData" classes for future use.

---
+++ src/main/java/dacd/navarro/model/Location.java
@@ -0,0 +1,27 @@
+package dacd.navarro.model;
+
+public class Location {
+    private String name;
+    private Double latitude;
+    private Double longitude;
+
+    public Location(String name, Double latitude, Double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+}
---
+++ src/main/java/dacd/navarro/model/WeatherData.java
@@ -0,0 +1,70 @@
+package dacd.navarro.model;
+
+public class WeatherData {
+    private String name;
+    private String location;
+    private String date;
+    private double temperature;
+    private int humidity;
+    private int clouds;
+    private double windSpeed;
+    private String description;
+    private double precipitation;
+
+    public WeatherData(String name, String location, String date, double temperature, double precipitation, int humidity, int clouds, double windSpeed, String description) {
+        this.name = name;
+        this.location = location;
+        this.date = date;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.clouds = clouds;
+        this.windSpeed = windSpeed;
+        this.description = description;
+        this.precipitation = precipitation;
+    }
+
+    public double getPrecipitation() {
+        return precipitation;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getLocation() {
+        return location;
+    }
+
+
+    public String getDate() {
+        return date;
+    }
+
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+
+    public int getClouds() {
+        return clouds;
+    }
+
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+}
