2508c0ae47e92391fc110a43864b70f554488d3b
maria <marianava171004@gmail.com>
2023-11-17 20:15:36 +0000

    Called 'execute()' function in main and set it to run periodically.

--- src/main/java/dacd/navarro/control/Main.java
+++ src/main/java/dacd/navarro/control/Main.java
@@ -1,7 +1,27 @@
 package dacd.navarro.control;
 
+import java.text.ParseException;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ParseException {
+
+        Timer timer = new Timer();
+
+        long delay = 0;
+        long period = 6 * 60 * 60 * 1000;
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+
+                    WeatherDataController.execute();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, delay, period);
     }
 }
