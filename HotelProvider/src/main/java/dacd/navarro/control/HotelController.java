3eed45109b21d9f2477e7c8337d4d4d784fdb3d5
maria <marianava171004@gmail.com>
2024-01-01 22:32:50 +0000

    Created 'HotelController' class with 'execute()' function and called necessary methods.

---
+++ HotelProvider/src/main/java/dacd/navarro/control/HotelController.java
@@ -0,0 +1,56 @@
+package dacd.navarro.control;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import javax.jms.JMSException;
+import java.io.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelController {
+    public static void execute(String apiKey) throws JMSException {
+        List<String> islands;
+        Provider dataProvider = new HotelProvider();
+        islands = readFile();
+        for(int i = 0; i < islands.size(); i++){
+            int regionId = HotelApiConnector.getRegionId(islands.get(i), apiKey);
+            String response = HotelApiConnector.getHotels(regionId, apiKey, "2024-09-09", "2024-10-10", 1);  //como hago que el cliente ponga esto
+            List<String> hotelSerializedList = dataProvider.getHotelInfo(response, islands.get(i));
+
+            for(String hotelObjectSerialized : hotelSerializedList){
+
+                MessageSender.messageToBroker(hotelObjectSerialized);
+                System.out.println("Message sent:" + hotelObjectSerialized);
+            }
+            System.out.println("Hotel list from " + islands.get(i) + " added.");
+
+        }
+    }
+
+
+    public static List<String> readFile(){
+        List<String> islands = new ArrayList<>();
+        try (InputStream inputStream = HotelController.class.getClassLoader().getResourceAsStream("Locations.csv")) {
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                CSVReader reader = new CSVReader(inputStreamReader);
+
+                String[] line;
+                while ((line = reader.readNext()) != null) {
+                    String name = line[0];
+                    islands.add(name);
+                }
+
+            } else {
+                throw new IOException("Locations.csv could not be loaded.");
+            }
+        } catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return islands;
+    }
+}
--- HotelProvider/src/main/java/dacd/navarro/control/Main.java
+++ HotelProvider/src/main/java/dacd/navarro/control/Main.java
@@ -1,7 +1,26 @@
 package dacd.navarro.control;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Timer timer = new Timer();
+
+        long delay = 0;
+        long period = 6 * 60 * 60 * 1000;
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    String apiKey = args[0];
+                    HotelController.execute(apiKey);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, delay, period);
     }
 }
